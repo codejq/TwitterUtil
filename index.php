1ae09e361bb0c8787afea8627d4e74439aae3ae9
ali <codejq@gmail.com>
2022-06-15 11:17:47 -0700

    First commit

--- index.php
+++ index.php
@@ -3,24 +3,22 @@ session_start();
 //add autoload note:do check your file paths in autoload.php
 require "./vendor/autoload.php";
 require "./vendor/TwitterUtil/autoload.php";
+
 use TwitterUtil\TwitterOAuthorize;
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
-define('CONSUMER_KEY' , $_ENV['CONSUMER_KEY']);
-define('CONSUMER_SECRET' , $_ENV['CONSUMER_SECRET']);
-$twitterUtil = new TwitterOAuthorize(CONSUMER_KEY ,CONSUMER_SECRET , "http://ubuntu:81/");
-
+define('CONSUMER_KEY', $_ENV['CONSUMER_KEY']);
+define('CONSUMER_SECRET', $_ENV['CONSUMER_SECRET']);
+$twitterUtil = new TwitterOAuthorize(CONSUMER_KEY, CONSUMER_SECRET, "http://ubuntu:81/");
 
 
 //this code will run when returned from twiter after authentication
-if (isset($_SESSION['oauth_token']) && $_SESSION['token']  !== $_REQUEST['oauth_token']) {
+if (isset($_SESSION['oauth_token']) && $_SESSION['token'] !== $_REQUEST['oauth_token']) {
     $twitterUtil->StoreToken();
-
 } else {
-
     $twitterUtil->Authorize();
-
 }
 
 
